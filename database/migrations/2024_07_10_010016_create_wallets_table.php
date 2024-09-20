2738e43ee05e9e62f4718d1c59bfdb75060408be
MatIketani <matfmacedo@gmail.com>
2024-09-20 13:08:08 -0300

    Ajustes no Banco de Dados

--- database/migrations/2024_07_10_010016_create_wallets_table.php
+++ database/migrations/2024_07_10_010016_create_wallets_table.php
@@ -19,7 +19,8 @@ return new class extends Migration
 
             $table->foreign('user_id')
                 ->references('id')
-                ->on('users');
+                ->on('users')
+                ->onDelete('cascade');
 
             $table->unsignedInteger('credits')
                 ->default(0);
